3e66eec006abb0082ab4271c1679c968b8ff0dc1
cosmoden69 <cosmoden@hotmail.com>
2022-05-19 18:46:24 +0900

    ..

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,6 +25,10 @@ namespace YLW_WebClient
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
+
+                Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+                AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Application_UnhandledException);
+
                 MainForm = new frmMain();
                 Application.Run(MainForm);
                 //Application.Run(new Form2());   //테스트용
@@ -32,6 +37,16 @@ namespace YLW_WebClient
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
+
+        private static void Application_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString());
         }
     }
 }
